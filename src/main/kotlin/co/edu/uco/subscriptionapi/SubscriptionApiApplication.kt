4ccb0436d4bd91538c158d118416d33ccf526e12
Julian Diaz <diazjulian460@gmail.com>
2024-02-15 18:45:45 -0500

    Creación del proyecto

---
+++ src/main/kotlin/co/edu/uco/subscriptionapi/SubscriptionApiApplication.kt
@@ -0,0 +1,11 @@
+package co.edu.uco.subscriptionapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubscriptionApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SubscriptionApiApplication>(*args)
+}
