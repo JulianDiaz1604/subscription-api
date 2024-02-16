cfdec90758aadc006089bcd2970cf54479e9352f
Julian Diaz <diazjulian460@gmail.com>
2024-02-15 19:03:00 -0500

    Cambio de Kotlin a Java

---
+++ src/main/java/co/edu/uco/subscriptionapi/SubscriptionApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.subscriptionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionApiApplication.class, args);
+	}
+
+}
