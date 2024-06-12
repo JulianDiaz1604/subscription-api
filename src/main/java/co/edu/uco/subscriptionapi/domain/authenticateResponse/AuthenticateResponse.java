f72eefd35ffbfeaa7687c27ff2ead58f5936646b
Diaz <diazjulian460@gmail.com>
2024-06-12 02:09:54 -0500

    Error raro de convertir respuesta de autenticación solucionado

--- src/main/java/co/edu/uco/subscriptionapi/domain/authenticateResponse/AuthenticateResponse.java
+++ src/main/java/co/edu/uco/subscriptionapi/domain/authenticateResponse/AuthenticateResponse.java
@@ -1,11 +1,18 @@
 package co.edu.uco.subscriptionapi.domain.authenticateResponse;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
 public class AuthenticateResponse {
 
+    @JsonProperty("myUserId")
     private UUID myUserId;
+
+    @JsonProperty("isAdmin")
     private boolean isAdmin;
+
+    @JsonProperty("token")
     private String token;
 
     public AuthenticateResponse(UUID myUserId, boolean isAdmin, String token) {
